4ad2628597230cc8af06d26b07bc3949e99328bd
Iwo Brzyski <49171448+Iwo98@users.noreply.github.com>
2019-10-31 19:25:16 +0100

    moving shooter

--- GRA/Form1.Designer.cs
+++ GRA/Form1.Designer.cs
@@ -29,12 +29,56 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 131);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(779, 567);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(779, 100);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1006, 721);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- GRA/Form1.cs
+++ GRA/Form1.cs
@@ -7,14 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Timers;
+
 
 namespace GRA
 {
     public partial class Form1 : Form
     {
+        Boolean win = false;
+        Graphics g;
+        Shooter shooter = new Shooter();
+        Pen shooterPen = new Pen(Color.White, 1);
+
         public Form1()
         {
             InitializeComponent();
+            g = panel1.CreateGraphics();
+            
+        }
+
+            private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+            System.Windows.Forms.Timer t = new System.Windows.Forms.Timer();
+            t.Tick += new EventHandler(timer1_Tick);
+            t.Start();
+
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        public void drawing()
+        { 
+            g.Clear(Color.Black);
+            g.DrawRectangle(shooterPen, shooter.x, shooter.y, shooter.width, shooter.height);
+            shooter.move();
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            drawing();
         }
     }
 }
